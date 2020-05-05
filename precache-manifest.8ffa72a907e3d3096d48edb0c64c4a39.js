28568fc89470ff617048aa8196bddab658c35209
mediheb1920 <ihebhichri11@gmail.com>
2020-05-05 12:49:33 +0100

    Updates

---
+++ precache-manifest.8ffa72a907e3d3096d48edb0c64c4a39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d556d25dfbea800f80074c647dfcc1d",
+    "url": "/EatEasy-Delivery-Food/index.html"
+  },
+  {
+    "revision": "4bd46290915b9619f6df",
+    "url": "/EatEasy-Delivery-Food/static/css/main.d1e0af6f.chunk.css"
+  },
+  {
+    "revision": "67ae33743db9fa694038",
+    "url": "/EatEasy-Delivery-Food/static/js/2.eb809347.chunk.js"
+  },
+  {
+    "revision": "e1438dfb798df3a2753ba9f0e5ed6bc4",
+    "url": "/EatEasy-Delivery-Food/static/js/2.eb809347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd46290915b9619f6df",
+    "url": "/EatEasy-Delivery-Food/static/js/main.d84e2bcf.chunk.js"
+  },
+  {
+    "revision": "0b134ca4ad50afd76e87",
+    "url": "/EatEasy-Delivery-Food/static/js/runtime-main.cb2491c7.js"
+  },
+  {
+    "revision": "b4d9ea112993470387f22f490ea326a6",
+    "url": "/EatEasy-Delivery-Food/static/media/food.b4d9ea11.jpeg"
+  },
+  {
+    "revision": "8f35a40403a84631c4125c4f1859c7a6",
+    "url": "/EatEasy-Delivery-Food/static/media/twitter.8f35a404.png"
+  },
+  {
+    "revision": "ac9bb33b738d955b674f91019f9f0056",
+    "url": "/EatEasy-Delivery-Food/static/media/undraw.ac9bb33b.svg"
+  }
+]);
